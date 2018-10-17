f0681f23c5f76357b701b9392a436a64abfc6f79
Petro Grebenyuk <grebenyuk.java@gmail.com>
2018-10-17 13:43:14 +0300

    changed basket

--- README.md
+++ README.md
@@ -6,6 +6,6 @@ Installation:
 1. Open terminal/command prompt.
 2. Clone repository with project from https://github.com/pgrebenyuk/myapp.git.
 3. Navigate to the "myapp" directory.
-4. Use command "mvn package" to compile, test and package project into a war file.
+4. Use command "mvn package" to compile and package project into a war file.
 5. Start Tomcat.
 6. Deploy war archive to tomcat server.
--- src/main/webapp/scriptshop.js
+++ src/main/webapp/scriptshop.js
@@ -1,16 +1,15 @@
 $(function() {
     var count = 0;
     $('#result').load('http://localhost:8080/myapp/shop/items #items');
-    $('#container').load('http://localhost:8080/myapp/shop/basket');
     $('#result').on('click',':checkbox', function() {
         var id = this.id;
 
         if (this.checked) {
             var name = this.labels[0].textContent;
             count++;
-            $('#basket').append('<p id=' + id + '>' + name + '</p>');
+            $('#container').append('<p id=' + id + '>' + name + '</p>');
         } else {
-            var elem = '#basket #'+id;
+            var elem = '#container #'+id;
             count--;
             $(elem).remove();
         }
@@ -19,5 +18,26 @@ $(function() {
         } else {
             $('#aside').hide();
         }
-    })
+    });
+    $("#openBasket").click(function() {
+        var items = $('#container p');
+        var itemsString = '';
+        items.each(function (index) {
+            if (index == 0) {
+                itemsString += $(this).text();
+            } else {
+                itemsString += ',' + $(this).text();
+            }
+            console.log(itemsString);
+        });
+        $('#items').value()
+        //$.ajax({
+        //    url: 'http://localhost:8080/myapp/shop/basket',
+        //    data: itemsString,
+        //}).then(function (result) {
+        //    console.log('result', result);
+        //}).catch(function (err) {
+        //    console.log('err', err)
+        //})
+    });
 })
