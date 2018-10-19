2732acec69e43b7e6d94ccd3a9bdbc1797f27d93
Petro Grebenyuk <grebenyuk.java@gmail.com>
2018-10-19 09:27:07 +0300

    added promise to ajax

--- src/main/webapp/scriptbasket.js
+++ src/main/webapp/scriptbasket.js
@@ -6,24 +6,18 @@ $(function() {
     };
 
     $("#basketButton").click(function() {
-        var serializedData = $('#basket p').serialize();
-        $.ajax({
-            type : "POST",
-            url : "http://localhost:8080/myapp/buyService",
-            data : serializedData,
-            contentType: "charset=UTF-8",
-            success: function (response) {
-                console.log("Success", + response.statusCode);
-            },
-            statusCode: {
-                201: function() {
-                    var url = "http://localhost:8080/myapp/shop/success";
-                    $(location).attr('href',url);
-                }
-            },
-            error: function (e) {
-                console.log("Server error - " + e);
+        $.post("http://localhost:8080/myapp/buyService")
+        .then(function(data, textStatus, jqHXR){
+            if(jqHXR.status == 201){
+                var url = "http://localhost:8080/myapp/shop/success";
+                $(location).attr('href',url);
+            }else {
+                console.log(jqHXR.status);
             }
-        });
-    });
+        })
+        .fail(function(){
+            console.log('error:');
+        })
+    })
 })
+
