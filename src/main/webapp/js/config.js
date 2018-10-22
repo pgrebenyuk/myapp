061f3493a24db75f4558aab13cabe1b77d638faf
Petro Grebenyuk <grebenyuk.java@gmail.com>
2018-10-22 14:29:23 +0300

    added RequireJS

--- src/main/webapp/js/config.js
+++ src/main/webapp/js/config.js
@@ -1,7 +1,3 @@
 requirejs.config({
-    //baseUrl: '/js',
-    paths: {
-        'modyleshop': 'http://localhost:8080/myapp/js/modyleshop',
-        'modylebasket': 'http://localhost:8080/myapp/js/modylebasket'
-    }
+    baseUrl: '/myapp/js'
 })
